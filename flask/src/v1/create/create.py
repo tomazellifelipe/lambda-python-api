e61a02235438e1e8c3a51e5c7465b353898d8f4d
Felipe Tomazelli <fefetl08@gmail.com>
2023-11-01 21:53:39 -0300

    feat: add return event to decorator

--- flask/src/v1/create/create.py
+++ flask/src/v1/create/create.py
@@ -16,22 +16,21 @@ def event_validator(model):
         @wraps(f)
         def validator(*args, **kwargs):
             try:
-                _ = model(**request.json)
+                event = model(**request.json)
             except ValidationError as e:
                 logging.debug("bad request", e.errors()[0])
                 return build_response(
                     http_status=HTTPStatus.BAD_REQUEST,
                     body={"message": "bad request", "errors": e.errors()},
                 ), 400
-            return f(*args, **kwargs)
+            return f(event, *args, **kwargs)
         return validator
     return wrapper
 
 
 @router.route("", methods=["POST"])
 @event_validator(model=Item)
-def create():
-    item = Item(**request.json)
-    message = f"item {item.name} was created with price R${item.price:.2f}"
+def create(event):
+    message = f"item {event.name} was created with price R${event.price:.2f}"
     logging.debug('Item created', item.model_dump())
     return build_response(http_status=HTTPStatus.OK, body={"message": message}), 200
