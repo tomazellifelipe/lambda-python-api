89937e9ec45e237bf1a33ea19a8872d8d5b0014a
Felipe Tomazelli <fefetl08@gmail.com>
2023-11-01 21:11:09 -0300

    feat: add event validator decorator

--- flask/src/v1/create/create.py
+++ flask/src/v1/create/create.py
@@ -1,3 +1,4 @@
+from functools import wraps
 import logging
 from http import HTTPStatus
 
@@ -10,7 +11,24 @@ from flask import Blueprint, request
 router = Blueprint(name="create", import_name=__name__)
 
 
+def event_validator(model):
+    def wrapper(f):
+        @wraps(f)
+        def validator(*args, **kwargs):
+            try:
+                _ = model(**request.json)
+            except ValidationError as e:
+                return build_response(
+                    http_status=HTTPStatus.BAD_REQUEST,
+                    body={"message": "bad request from inside wrapper", "errors": e.errors()},
+                )
+            return f(*args, **kwargs)
+        return validator
+    return wrapper
+
+
 @router.route("", methods=["POST"])
+@event_validator(model=Item)
 def create():
     try:
         item = Item(**request.json)
@@ -20,5 +38,5 @@ def create():
             body={"message": "bad request", "errors": e.errors()},
         )
     message = f"item {item.name} was created with price R${item.price:.2f}"
-    logging.debug('Item created', item.model_dump())
+    print('Item created', item.model_dump())
     return build_response(http_status=HTTPStatus.OK, body={"message": message})
