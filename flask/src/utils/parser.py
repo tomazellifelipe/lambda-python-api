39ceb081c79830720a8f568770c49beb49fcc1a5
Felipe Tomazelli <fefetl08@gmail.com>
2023-11-06 09:36:55 -0300

    feat: simplify event parse and validator

--- flask/src/utils/parser.py
+++ flask/src/utils/parser.py
@@ -1,27 +1,13 @@
 import logging
-from functools import wraps
-from http import HTTPStatus
+from typing import Any
 
 from pydantic import BaseModel, ValidationError
 
-from flask import request
-from src.utils.http_response import build_response
 
-
-def event_validator(model: BaseModel) -> callable:
-    def wrapper(f):
-        @wraps(f)
-        def validator(*args, **kwargs):
-            try:
-                event = model(**request.json)
-            except ValidationError as e:
-                logging.debug("bad request", {"errors": e.errors(), "payload": request.json})
-                return (
-                    build_response(body={"errors": e.errors()}),
-                    HTTPStatus.BAD_REQUEST,
-                )
-            return f(event, *args, **kwargs)
-
-        return validator
-
-    return wrapper
+def parse_and_validate(payload: dict[str, Any], model: BaseModel) -> BaseModel:
+    try:
+        event = model(**payload)
+    except:
+        logging.debug("Errors", payload)
+        raise ValidationError
+    return event
--- flask/src/v1/routes.py
+++ flask/src/v1/routes.py
@@ -1,17 +1,18 @@
 import logging
 from http import HTTPStatus
 
-from flask import Blueprint, Response
 from src.utils.http_response import build_response
-from src.utils.parser import event_validator
+from src.utils.parser import parse_and_validate
 from src.v1.models import Item
+
+from flask import Blueprint, Response, request
 
 router = Blueprint(name="api", import_name=__name__, url_prefix="/api/v1")
 
 
 @router.route("/create", methods=["POST"])
-@event_validator(model=Item)
-def create(event: Item) -> tuple[Response, int]:
-    message = f"item {event.name} was created with price R${event.price:.2f}"
+def create() -> tuple[Response, int]:
+    event: Item = parse_and_validate(request.json)
+    message: str = f"item {event.name} was created with price R${event.price:.2f}"
     logging.debug("Item created", event.model_dump())
     return build_response(body={"message": message}), HTTPStatus.OK
